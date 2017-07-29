7400c672bb2ef2949cc56203012e445883c2b22d
PsyTian <kyzxt_123@126.com>
2017-07-29 23:26:40 +0800

    复习上传

---
+++ p2.py
@@ -0,0 +1,7 @@
+def my_last(ky):
+    return ky[-2]
+print(my_last([1,2,3,4,'qwe','asd','zxc','wer',5,6]))
+print(my_last([1,2,3,4,'qwe','asd','zxc','wer']))
+print(my_last([1,2,3,4,'qwe','asd','zxc']))
+print(my_last([1,2,3,4,'qwe']))
+print(my_last([1,2,3,4]))
