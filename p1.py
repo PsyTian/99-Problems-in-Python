0268c290d466f8536f1fd6fe5594f4ba1353dc72
PsyTian <kyzxt_123@126.com>
2017-07-29 23:15:03 +0800

    改变思路首先要学会新方法哇！

---
+++ p1.py
@@ -0,0 +1,6 @@
+def my_last(ky):
+    return ky[-1]
+print(my_last([1,2,3,4,'qwe','asd','zxc','wer',5,6]))
+print(my_last([1,2,3,4,'qwe','asd','zxc','wer']))
+print(my_last([1,2,3,4,'qwe','asd','zxc']))
+print(my_last([1,2,3,4,'qwe']))
