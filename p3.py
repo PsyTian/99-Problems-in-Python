871a3ecbcd9430b7fa2fddba38f82f7eb8a72aff
PsyTian <kyzxt_123@126.com>
2017-08-02 22:38:54 +0800

    说不清，很玄幻。

---
+++ p3.py
@@ -0,0 +1,8 @@
+def function(list,k):
+    return list[k-1]
+
+
+    
+print(function([1,2,3,4,'qwe','asd','zxc','wer',5,6],5))
+print(function([1,2,3,4,'qwe','asd','zxc','wer',5,6],6))
+print(function([1,2,3,4,'qwe','asd','zxc','wer',5,6],9))
