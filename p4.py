a30b50186224d823f00a4b751d13f9849a9003ca
PsyTian <kyzxt_123@126.com>
2017-08-02 22:40:17 +0800

    第二种一点也不好啊，一点也不函数？

---
+++ p4.py
@@ -0,0 +1,15 @@
+def my_list(ky):
+    return len(ky)
+print(my_list([1,2,3,4,'qwe','asd','zxc','wer',5,6,'sdf']))
+
+
+
+#第二种
+my_list = [1,2,3,4,'qwe','asd','zxc','wer',5,6,'sdf','zxc','wer']
+
+k = 0
+for each in my_list:
+    k = k + 1
+print(k)
+
+
