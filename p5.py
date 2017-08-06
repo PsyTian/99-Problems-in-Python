b800b514b3b73f093f3eff5afe490935ef5ee87b
PsyTian <kyzxt_123@126.com>
2017-08-07 02:22:51 +0800

    然而就是很刺激啊!

---
+++ p5.py
@@ -0,0 +1,26 @@
+#Reverse a list.
+#翻转一个列表里所有元素的顺序。
+
+#第一种
+def my_list_reverse(alist):
+    blist = []
+    for each in alist:
+        blist.insert(0, each)   #list.insert 在某一特定位置前面添加一个数据项。
+    return blist
+
+#test
+print(my_list_reverse([1,2,3,4,'qwe','asd','zxc','wer',5,6,'sdf']))	
+print(my_list_reverse([1,2,3,4,5,6]))
+print(my_list_reverse(['qwe','asd','zxc','wer','sdf']))
+
+#第二种
+def my_list_reverse(alist):
+	blist = list(alist)
+	return blist.reverse() or blist
+
+#test
+print(my_list_reverse([1,2,3,4,'qwe','asd','zxc','wer',5,6,'sdf']))	
+print(my_list_reverse([1,2,3,4,5,6]))
+print(my_list_reverse(['qwe','asd','zxc','wer','sdf']))
+
+
